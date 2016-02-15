3aa6ac5f465377184bf36592ad16d49a05038582
Alex0007 <Alex0007lolpvp@gmail.com>
2016-02-15 21:14:05 +0500

    init config.mod.js

---
+++ config.mod.js
@@ -0,0 +1,11 @@
+/**
+ * You can override config.js vars in this file
+ * After changes in this file run `git update-index --assume-unchanged config.mod.js` command to ignore changes
+ */
+export default {
+  env: {
+    testing: {},
+    development: {},
+    production: {}
+  }
+}
