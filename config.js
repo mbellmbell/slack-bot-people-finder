7df205126c25070dfd74fedc8a2c3b61565aeb76
Alex0007 <Alex0007lolpvp@gmail.com>
2016-02-18 05:24:30 +0500

    production env

--- config.js
+++ config.js
@@ -4,7 +4,12 @@ let config = {
   env: {
     testing: {},
     development: {},
-    production: {}
+    production: {
+      WEBHOOK_URL: 'https://hooks.slack.com/services/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',
+      CHANNEL: 'general',
+      BOT_NAME: 'Emailfinder Bot',
+      API_TOKEN: 'xoxp-21459716064-21459923766-21466998982-b015a2e4ec'
+    }
   }
 }
 
